279d67f0b4796e2122ff4ff891b02e0dedfd2a8c
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 15:10:05 -0500

    add put method on category route to update category name by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
+    const categoryData = await Category.update(req.body, {
       where: { id: req.params.id }
     });
     if (!categoryData) {
