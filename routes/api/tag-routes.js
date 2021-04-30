be3f38d0855f9f62e4e0f75c546e050f8a6ce5c1
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 14:49:34 -0500

    update tag routes to get all tags with associated products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
