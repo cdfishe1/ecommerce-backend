0756f05b81385a072d285f5ec2c436e39649125d
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 16:05:07 -0500

    add delete function to products where record destroyed on id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,8 +117,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
