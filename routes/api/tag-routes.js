253eb8d5775e382eb0f6b195f982a27a499caf86
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 15:02:45 -0500

    add put method to tag routes to update tag name by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,8 +47,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
