68d341f9fd7197b7f5d77efc4488f43ae19e293d
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 14:57:51 -0500

    add post method to tag route to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,8 +37,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
