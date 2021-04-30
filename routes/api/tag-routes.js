d8e4e6d1d9f33d1d4b499222bbe8fdf5d1483dd0
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 14:53:32 -0500

    add get by id to tag routes including product info

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,9 +18,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
