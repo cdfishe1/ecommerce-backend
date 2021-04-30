adb6eddb423a6e06e5870826c41b79039b3a2994
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-30 10:17:48 -0500

    add associations for models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: category_id
+});
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  foreignKey: category_id
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
 module.exports = {
   Product,
