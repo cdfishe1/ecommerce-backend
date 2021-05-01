fab2ec27377f86587cdc6d1ede309298b3b7985a
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-01 14:56:09 -0500

    remove comment from product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -53,7 +53,9 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+    
   Product.create(req.body)
+  
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
